ffbfee5f828385e070db26c2a2e81c34fa40ef8c
Amirmohammad Molaei <165934546+AmirMohammadMolaei@users.noreply.github.com>
2025-03-12 02:43:03 +0330

    responsive landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,18 @@ import { TextGenerateEffect } from "@/components/common/text-generate-effect";
 export default function Home() {
   return (
     <BackgroundBeamsWithCollision className="bg-transparent !h-fit">
-      <div className="w-full h-fit font-Cinzel text-8xl flex flex-wrap gap-y-8 *:w-full pt-32 pb-16">
-        <TextGenerateEffect words={"Human"} className="w-full" />
-        <TextGenerateEffect
-          words={"Stories & Ideas"}
-          className="w-full -mt-20"
-        />
-        <p className="font-sans text-2xl w-full">
+      <div className="w-full h-fit font-Cinzel max-sm:text-4xl max-md:text-6xl max-xl:text-7xl text-8xl flex flex-wrap max-md:justify-center gap-y-8 *:w-full max-lg:pt-16 pt-32 pb-16">
+        <div>
+          <TextGenerateEffect
+            words={"Human"}
+            className="w-full max-md:text-center"
+          />
+          <TextGenerateEffect
+            words={"Stories & Ideas"}
+            className="w-full max-md:text-center"
+          />
+        </div>
+        <p className="font-sans max-xl:text-xl text-2xl w-full max-md:text-center">
           A place to read, write, and deepen your understanding
         </p>
         <Button
