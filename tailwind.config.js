a9c5eb2458d73e4d273af55a3d35afceb82fff2d
Amirmohammad Molaei <165934546+AmirMohammadMolaei@users.noreply.github.com>
2025-03-31 18:58:11 +0330

    add tailwind old version

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+import { heroui } from "@heroui/react";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Hero-UI
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        aurora: "aurora 60s linear infinite",
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
+    },
+    fontFamily: {
+      Habibi: "Habibi",
+      Cinzel: "Cinzel",
+    },
+  },
+  plugins: [heroui()],
+};
